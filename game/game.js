e5413e0f7bddeed0903f16b74c27888f3c311434
Capi Etheriel <barraponto@gmail.com>
2013-01-26 13:31:19 -0200

    umahora: minimal working code

--- game/game.js
+++ game/game.js
@@ -11,5 +11,15 @@ require.config({
 
 require(["jquery", "crafty"], function($, Crafty) {
   $(document).ready(function(){
+    Crafty.init(600, 420);
+    Crafty.background('rgb(0,0,0)');
+
+    Crafty.scene('level1', function() {
+      Crafty.e('player1, DOM, 2D, Multiway, Color')
+      .attr({x: 0, y: 0, w: 10, h: 10})
+      .multiway(5, {W: -90, A: 180, S: 90, D: 0})
+      .color('#ffffff');
+    });
+    Crafty.scene('level1');
   });
 });
