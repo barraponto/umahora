45628c566eb6ff1d4cba2ad88da3be053540fa76
Capi Etheriel <barraponto@gmail.com>
2013-01-26 13:29:33 -0200

    project: integrate require.js

--- game/game.js
+++ game/game.js
@@ -1,5 +1,15 @@
-$(document).ready(function(){
-  Crafty.init(600, 420);
-  Crafty.canvas.init();
-  Crafty.background('rgb(0,0,0)')
-})
+require.config({
+  baseUrl: '.',
+  paths: {
+    // Both jQuery and Crafty hardcode the exported module name.
+    "jquery": "js/vendor/jquery-1.9.0",
+    "crafty": "js/vendor/crafty-0.5.4-c177c5e",
+    // Everything else should come from vendor.
+    "vendor": "js/vendor"
+  },
+});
+
+require(["jquery", "crafty"], function($, Crafty) {
+  $(document).ready(function(){
+  });
+});
