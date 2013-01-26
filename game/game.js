9e411c216b7f256189747e3e9834ccfb52fc5f03
Capi Etheriel <barraponto@gmail.com>
2013-01-26 18:46:15 -0200

    project: added backbone, renamed paths

--- game/game.js
+++ game/game.js
@@ -4,12 +4,18 @@ require.config({
     // Both jQuery and Crafty hardcode the exported module name.
     "jquery": "js/vendor/jquery-1.9.0",
     "crafty": "js/vendor/crafty-0.5.4-c177c5e",
-    // Everything else should come from vendor.
-    "vendor": "js/vendor"
+    "lodash": "js/vendor/lodash-1.0.0-rc3",
+    "backbone": "js/vendor/backbone-0.9.10"
   },
+  shim: {
+    "backbone": {
+      "deps": ["lodash", "jquery"],
+      "exports": "Backbone"
+    }
+  }
 });
 
-require(["jquery", "crafty"], function($, Crafty) {
+require(["lodash", "jquery", "crafty", "backbone"], function(_, $, Crafty, Backbone) {
   $(document).ready(function(){
     Crafty.init(600, 420);
     Crafty.background('rgb(0,0,0)');
